74f79811cf1148fd7de8b7cd4e38f3641721d57e
Tingfan Wu <tingfan.wu@umbocv.com>
2015-10-08 16:42:06 +0800

    test ok on umbomars

--- selenium/check-rtc-delay.py
+++ selenium/check-rtc-delay.py
@@ -15,31 +15,32 @@ import selenium.webdriver.firefox.firefox_profile as firefox_profile
 # enable settings in "about:config"
 firefoxProfile = webdriver.FirefoxProfile(); # OSX ref ~/Library/Application Support/Firefox/Profiles/*/prefs.js
 firefoxProfile.set_preference('canvas.capturestream.enabled', True);
+firefoxProfile.set_preference('media.peerconnection.ice.tcp', True);
 firefoxProfile.update_preferences()
 
 #launch remote instance
-driver = webdriver.Remote(command_executor='http://127.0.0.1:4444/wd/hub', desired_capabilities=DesiredCapabilities.FIREFOX, browser_profile=firefoxProfile)
 
 # go to the google home page
-driver.get("http://g.umbocv.com:8000/kms-client-js/rtc_source_from_file.html")
+opts = [ (0,0),(0,1),(1,0),(1,1)]
 
+for i in range(len(opts)):
+    driver = webdriver.Remote(command_executor='http://127.0.0.1:4444/wd/hub', desired_capabilities=DesiredCapabilities.FIREFOX, browser_profile=firefoxProfile)
+    url="http://g.umbocv.com:8000/kms-client-js/rtc.html?forceICETCP=%d&notrickle=%d" % opts[i]
+    print(url)
+    driver.get(url);
+    timing = [];
+    try:
+        # we have to wait for the page to refresh, the last thing that seems to be updated is the title
+        WebDriverWait(driver, 30).until(EC.title_contains("Auto"))
+        print driver.title
 
-timing = [];
-try:
-    # we have to wait for the page to refresh, the last thing that seems to be updated is the title
-    WebDriverWait(driver, 30).until(EC.title_contains("Auto"))
-    print driver.title
+        while driver.execute_script("return document.getElementById('videoInput').error") ==None and \
+                not driver.execute_script("return document.getElementById('videoInput').onend"):
+            message= driver.find_element_by_id('message').text
+            timing.append(map(float,message.split()[2].split(',')))
 
-    while driver.execute_script("return document.getElementById('videoInput').error") ==None and \
-            not driver.execute_script("return document.getElementById('videoInput').onend"):
-        message= driver.find_element_by_id('message').text
-        timing.append(map(float,message.split()[2].split(',')))
+    finally:
+        driver.quit()
 
-    driver.get_screenshot_as_file('screenshot.png')
-
-
-finally:
-    driver.quit()
-
-import numpy;
-numpy.array(timing).dump('timing.npz');
+    import numpy;
+    numpy.array(timing).dump('timing%d.npz' %i);
--- selenium/plot-delay.py
+++ selenium/plot-delay.py
@@ -1,32 +1,33 @@
 #!/usr/bin/env python
 import numpy
 
+for i in range(4):
+    timing_arr = numpy.load('timing%d.npz' % i)/1000;
 
-timing_arr = numpy.load('timing.npz')/1000;
+    sender = timing_arr[:,0];
+    receiver= timing_arr[:,1];
+    delay = timing_arr[:,2];
 
-sender = timing_arr[:,0];
-receiver= timing_arr[:,1];
-delay = timing_arr[:,2];
-print(timing_arr)
+    import matplotlib
+    matplotlib.use('Agg')
 
-import matplotlib
-matplotlib.use('Agg')
+    import pylab as p
+    p.subplot(1,2,1);
+    p.plot(sender, receiver,'.-')
+    maxt = numpy.nanmax(sender)
+    #p.axis([0,maxt,0,maxt]);
+    p.ylim([0, maxt]);
+    p.xlim([0, maxt]);
+    p.title('receiver vs sender');
+    p.xlabel('sender(s)')
+    p.ylabel('receiver(s)')
 
-import pylab as p
-p.subplot(1,2,1);
-p.plot(sender, receiver)
-maxt = numpy.nanmax(sender)
-#p.axis([0,maxt,0,maxt]);
-p.ylim([0, maxt]);
-p.xlim([0, maxt]);
-p.title('receiver vs sender');
-p.xlabel('sender(s)')
-p.ylabel('receiver(s)')
+    p.subplot(1,2,2);
+    p.plot(sender,delay,'.-');
+    p.xlabel('time(s)')
+    p.ylabel('delay')
 
-p.subplot(1,2,2);
-p.plot(sender,delay);
-p.xlabel('time(s)')
-p.ylabel('delay')
+    p.title('delay')
+    p.savefig("timing%d.png" % i)
 
-p.title('delay')
-p.savefig("timing.png")
+    print((numpy.diff(receiver[400:])==0).sum())
